5def1d61f75f8395e9d704fc9a951aa3f5e0f91a
shakeelsubratty <shakeel.subratty@gmail.com>
2019-08-10 20:55:03 +0100

    final windows dist

--- paycheck_optimsation.py
+++ paycheck_optimsation.py
@@ -20,6 +20,8 @@ Import Employees function - takes input_employees dataframe, creates and returns
 Params: input_employees - input dataframe
 Returns: employees - list of Employee objects
 """
+
+
 def import_employees(input_employees):
     employees = []
     for e in range(len(input_employees)):
@@ -38,6 +40,7 @@ def import_employees(input_employees):
         employees.append(employee)
     return employees
 
+
 """
 Import Payrolls function - takes input_payrolls dataframe between between start and end indices, and returns list of Payroll objects 
 
@@ -50,26 +53,28 @@ Returns:
 	len(input_payrolls) - number of payrolls imported
 	due_date_capacities - dictionary containing the assigned capacity per employee for each payroll due date
 """
+
+
 def import_payrolls(input_payrolls, start, end):
-    payrolls = {}	# Dictionary storying lists of Payrolls due on each due date
+    payrolls = {}  # Dictionary storing lists of Payrolls due on each due date
     due_dates = []
     due_date_capacities = {}
-	
-	# Find unique due dates between start and end date, sort dates ASC
+
+    # Find unique due dates between start and end date, sort dates ASC
     for p in range(start, end):
         due_date = input_payrolls.loc[p, 'Due date'].day
         due_dates.append(due_date)
     due_dates.sort()
-	
-	# Use due dates as keys in payrolls dict, assigning empty array to each
+
+    # Use due dates as keys in payrolls dict, assigning empty array to each
     for date in due_dates:
         payrolls[date] = []
-	
-	# Pull unique due dates and capacities from payroll spreadsheet. Drop null values and convert to lists.
+
+    # Pull unique due dates and capacities from payroll spreadsheet. Drop null values and convert to lists.
     unique_due_dates = input_payrolls['Unique Due Dates'].dropna().tolist()
     capacities = input_payrolls['Capacity per person'].dropna().tolist()
-	
-	# Check each due date has a capacity assigned
+
+    # Check each due date has a capacity assigned
     if len(unique_due_dates) != len(capacities):
         print("TABLEERROR")
         easygui.msgbox(
@@ -77,7 +82,12 @@ def import_payrolls(input_payrolls, start, end):
             "\nScript terminating early, please fix and try again.",
             "Missing table entry error")
         return
-	# Check each due date that exists in main payroll table has been given a capacity	
+
+    # Assign each capacity to appropriate due date in due_date_capacities dictionary
+    for i in range(0, len(capacities)):
+        due_date_capacities[unique_due_dates[i].day] = int(capacities[i])
+
+    # Check each due date that exists in main payroll table has been given a capacity
     if all(elem in payrolls.keys() for elem in due_date_capacities.keys()):
         print("TABLEERROR")
         easygui.msgbox(
@@ -85,14 +95,10 @@ def import_payrolls(input_payrolls, start, end):
             "\nScript terminating early, please fix and try again.",
             "Missing table entry error")
         return
-	
-	# Assign each capacity to appropriate due date in due_date_capacities dictionary
-    for i in range(0, len(capacities)):
-        due_date_capacities[unique_due_dates[i].day] = int(capacities[i])
 
     print(due_date_capacities)
-	
-	# For each payroll entry in range, attempt to create Payroll object and append to list on payrolls dictionary at that payroll's due date
+
+    # For each payroll entry in range, attempt to create Payroll object and append to list on payrolls dictionary at that payroll's due date
     for p in range(start, end):
         due_date = input_payrolls.loc[p, 'Due date'].day
         try:
@@ -113,6 +119,7 @@ def import_payrolls(input_payrolls, start, end):
         payrolls[due_date].append(payroll)
     return payrolls, len(input_payrolls), due_date_capacities
 
+
 """
 Allocate function - constructs Constraint Progromming model, establishes constraints and performs allocation procedure.
 	Allocation is carried out for each individual due date seperately, given the lists of employees and payrolls due on that day and 
@@ -129,67 +136,71 @@ Returns:
 	count - maintains a integer count of payrolls processed
 """
 
-def allocate(employees, payrolls, due_date_capacities, due_date, count):
-    toggle_do_not_reallocate = False	# Toggle whether to accept values from "Do not reallocate" column in Payrolls spreadsheet.
 
-    model = cp_model.CpModel()	# Constraint programming model and solver
-	
-	# Construct 2-D search space for allocations
+def allocate(employees, payrolls, due_date_capacities, due_date, count):
+    toggle_do_not_reallocate = False  # Toggle whether to accept values from "Do not reallocate" column in Payrolls spreadsheet.
+
+    model = cp_model.CpModel()  # Constraint programming model and solver
+
+    # Construct 2-D search space for allocations
     num_employees = len(employees)
     num_payrolls = len(payrolls)
     all_employees = range(num_employees)
     all_payrolls = range(num_payrolls)
-    x = []	# For each payroll, stores a possible employee
+    x = []  # For each payroll, stores a possible employee
     for p in all_payrolls:
-        y = []	# For each employee, stores CP model integer variable between (min, max). Solver adjusts these values between min/max to find a solution that satisfies constraints.
+        y = []  # For each employee, stores CP model integer variable between (min, max). Solver adjusts these values between min/max to find a solution that satisfies constraints.
         if toggle_do_not_reallocate:
             if payrolls[p].get_do_not_reallocate():
                 prev_emp_str = payrolls[p].get_previous_employee()
                 for e in all_employees:
                     if employees[e].get_name() == prev_emp_str:
-                        y.append(model.NewIntVar(1, 1, "x[%i,%i]" % (e, p)))	# If get_do_not_reallocate() == True, fix possible values of assignment for current employee to that payroll to (1,1) i.e. value must be 1.
+                        y.append(model.NewIntVar(1, 1, "x[%i,%i]" % (e,
+                                                                     p)))  # If get_do_not_reallocate() == True, fix possible values of assignment for current employee to that payroll to (1,1) i.e. value must be 1.
                         print("Do no reallocate: ", (e, p))
                     else:
-                        y.append(model.NewIntVar(0, 0, "x[%i,%i]" % (e, p)))	# For all other employees, fix value to 0
+                        y.append(model.NewIntVar(0, 0, "x[%i,%i]" % (e, p)))  # For all other employees, fix value to 0
             else:
-                for e in all_employees:											# If get_do_not_reallocate() == False, allow possible values of assignment to be 0 or 1.			
+                for e in all_employees:  # If get_do_not_reallocate() == False, allow possible values of assignment to be 0 or 1.
                     y.append(model.NewIntVar(0, 1, "x[%i,%i]" % (e, p)))
         else:
             for e in all_employees:
-                y.append(model.NewIntVar(0, 1, "x[%i,%i]" % (e, p)))			# If toggle_do_not_reallocate == False,  allow possible values of assignment to be 0 or 1 for all payrolls.
+                y.append(model.NewIntVar(0, 1, "x[%i,%i]" % (e,
+                                                             p)))  # If toggle_do_not_reallocate == False,  allow possible values of assignment to be 0 or 1 for all payrolls.
         print(y)
         x.append(y)
-	
-	# Remove any payrolls that are now outside each employee's two day turnover period
+
+    # Remove any payrolls that are now outside each employee's two day turnover period
     for e in all_employees:
         employees[e].clear_allocated_payrolls_2_days_prior(due_date)
-	print("Cleared employee two day turnover period")
+    print("Cleared employee two day turnover period")
 
-	# Initialising Constraints
+    # Initialising Constraints
     print("Initialising Constraints")
 
     # Constraint - Every payroll assigned to at least one employee
     [model.Add(sum(x[p][e] for e in all_employees) == 1)
      for p in all_payrolls]
-	
-	# Preparing repeatedly accessed dictionaries for easy access
-    employee_allocated_payroll_times = {} 						# Total time for currently allocated payrolls for each employee
-    employee_possible_payroll_processing_times = {}				# Total time for payrolls that could be assigned for each employee
-    employee_allocated_payroll_times_2days_from_due_date = {}	# Total time for currently allocated payrolls within two day period for each employee
-	for e in all_employees:
+
+    # Preparing repeatedly accessed dictionaries for easy access
+    employee_allocated_payroll_times = {}  # Total time for currently allocated payrolls for each employee
+    employee_possible_payroll_processing_times = {}  # Total time for payrolls that could be assigned for each employee
+    employee_allocated_payroll_times_2days_from_due_date = {}  # Total time for currently allocated payrolls within two day period for each employee
+    for e in all_employees:
         employee_allocated_payroll_times[employees[e].get_name()] = employees[e].get_allocated_payrolls_total_time()
         employee_possible_payroll_processing_times[employees[e].get_name()] = sum(
-            payrolls[p].get_processing_time() * x[p][e] for p in
+        payrolls[p].get_processing_time() * x[p][e] for p in
             all_payrolls)
         employee_allocated_payroll_times_2days_from_due_date[employees[e].get_name()] = employees[
             e].get_allocated_payrolls_time_2days_from_due_date()
-	
-	# Constraint - For each employee, possible processing times + currently allocated payroll times must be <= the employee's maxmimum amount of working time
+
+
+    # Constraint - For each employee, possible processing times + currently allocated payroll times must be <= the employee's maxmimum amount of working time
     [model.Add((employee_possible_payroll_processing_times[employees[e].get_name()] + employee_allocated_payroll_times[
         employees[e].get_name()] <= employees[e].get_max_hours())) for e in all_employees]
-	
-	# Constraint - For each employee, possible processing times must be <= capacity for current due date - processing time 
-	#	for payrolls assigned to that employee over two day turnover period.
+
+    # Constraint - For each employee, possible processing times must be <= capacity for current due date - processing time
+    #	for payrolls assigned to that employee over two day turnover period.
     [model.Add((employee_possible_payroll_processing_times[employees[e].get_name()] <= due_date_capacities[due_date] -
                 employee_allocated_payroll_times_2days_from_due_date[employees[e].get_name()])) for e in
      all_employees]
@@ -199,12 +210,12 @@ def allocate(employees, payrolls, due_date_capacities, due_date, count):
      all_payrolls for e in all_employees]
 
     print("Constraints intialised")
-	
-	# Find a solution to the model
+
+    # Find a solution to the model
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
-	
-	# If solution found, append assignments to Employee object fields by calling <Employee>.allocate_payrolls(allocate_payrolls, due_date)
+
+    # If solution found, append assignments to Employee object fields by calling <Employee>.allocate_payrolls(allocate_payrolls, due_date)
     print(status)
     if status == cp_model.FEASIBLE or status == cp_model.OPTIMAL:
         output = []
@@ -228,9 +239,10 @@ def allocate(employees, payrolls, due_date_capacities, due_date, count):
 Main function - reads input spreadsheets, carries out allocation procedure in blocks of 500 payrolls, and writes to output spreadsheets.
 
 """
-def main():
 
-	# Read input spreadsheet
+
+def main():
+    # Read input spreadsheet
     try:
         input_spreadsheet = pd.ExcelFile('Optimisation_Spreadsheet.xlsx')
     except FileNotFoundError:
@@ -240,14 +252,14 @@ def main():
             "\nEnsure a file named %s exists in the same directory as the script and try again." % (
                 "Optimisation_Spreadsheet_v3.xlsx"), "File not found error")
         raise
-	# Open output spreadsheet for writing
+    # Open output spreadsheet for writing
     try:
         writer = ExcelWriter('Optimisation_Allocation.xlsx', engine='openpyxl')
         book = load_workbook('Optimisation_Allocation.xlsx')
         writer.book = book
         writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-		
-		# Clear any existing values in columns
+
+        # Clear any existing values in columns
         df_empty_allocation = pd.DataFrame(
             columns=['Employee', 'Employee Technicality', 'Payroll', 'Payroll Technicality',
                      'Processing Time', 'Due date'])
@@ -262,17 +274,18 @@ def main():
             "\nEnsure a file named %s exists in the same directory as the script and try again." % (
                 "Optimisation_Allocation_v3.xlsx"), "File not found error")
         raise
-	
-	input_employees = pd.read_excel(input_spreadsheet, 'Employees')	# Reads from "Employee" worksheet
-    input_payrolls = pd.read_excel(input_spreadsheet, 'Payrolls')	# Reads from "Payrolls" worksheet
+
+    input_employees = pd.read_excel(input_spreadsheet, 'Employees')  # Reads from "Employee" worksheet
+    input_payrolls = pd.read_excel(input_spreadsheet, 'Payrolls')  # Reads from "Payrolls" worksheet
 
     employees = import_employees(input_employees)
     start_time = time.time()
     count = 0
     due_dates = []
-	total_number_of_payrolls = len(input_payrolls)
-	
-	# Perform Allocation procedure in blocks of 500 payrolls at a time
+    total_number_of_payrolls = len(input_payrolls)
+
+
+    # Perform Allocation procedure in blocks of 500 payrolls at a time
     for index in range(0, total_number_of_payrolls):
         if index == 0:
             payrolls, max_index, due_date_capacities = import_payrolls(input_payrolls, index, index + 500)
@@ -300,8 +313,8 @@ def main():
                            p.get_technicality(), p.get_processing_time(),
                            p.get_due_date()])
             output_payroll_ids.append(p.get_id())
-	
-	# Check to see if any payrolls were not allocated
+
+    # Check to see if any payrolls were not allocated
     failed_to_allocate = []
     for elem in input_payrolls['Payroll']:
         if elem not in output_payroll_ids:
@@ -342,7 +355,6 @@ def main():
     output_df.to_excel(writer, "Allocation")
     output_surface_df.to_excel(writer, "Emp vs Time Surface")
     writer.save()
-
 
 main()
 input('..')
